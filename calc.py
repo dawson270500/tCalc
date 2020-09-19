4d504c4c1c3ad88cf1fb7c8a532309eec5cd76ef
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-09-19 16:44:40 +0100

    Add files via upload

--- calc.py
+++ calc.py
@@ -3,6 +3,14 @@ memory = 0.0
 
 print("Type 'exit' to stop, 'help' for help")
 
+def numGet(str):
+	if str == "m":
+		return memory
+	elif str == "p":
+		return 3.14159
+	else:
+		return str
+
 while True:
 	inp = input(":> ")
 
@@ -15,77 +23,62 @@ while True:
 		print("You can add onto the last answer given, by typing '<operator><Number or value>'\nSupported operators:\n\t+\n\t-\n\t*\n\t/\nUsing pi or memory:\n\tto use pi, type p.\n\tTo set memory, type m after it gives the answer you want in memory. To acces memory type m in a calculation")
 	
 	elif inp != "m":
-		num1 = ""
-		numSet = False
-		num2 = ""
-		op = ""
-
-		for x in inp:
-			if x == "+" or x == "-" or x == "/" or x=="*":
-				op = x
-				if num1 == "":
-					num1 = str(lastAnswer)
-					if inp[1] == "m":
-						num2 = memory
-					
-					elif inp[1] =="p":
-						num2 = 3.14159
-					
-					else:
-						num2 = inp[1:]
-					break
-				
-				else:
-					numSet = True
-				continue
-			
-			if x.isnumeric() or x == "p" or x == "m":
-				if numSet == False:
-					if x == "p":
-						num1 = 3.14159
-					
-					elif x == "m":
-						num1 = memory
-
-					else:
-						num1 = num1+x
-				else:
-					if x == "p":
-						num2 = 3.14159
-					
-					elif x == "m":
-						num2 = memory
-
-					else:
-						num2 = num2+x
-				continue
+		if "+" in inp:
+			if "+" == inp[0]:
+				try:
+					lastAnswer = lastAnswer + float(numGet(inp[1:]))
+				except:
+					print("Invalid value")
 
 			else:
-				print("That isn't a number")
-				numSet = "e"
-				break
+				vals = inp.split("+")
+				lastAnswer = float(numGet(vals[0])) + float(numGet(vals[1]))
 
-		if numSet == "e":
-			continue			
+			print(lastAnswer)
+			continue
+		elif "-" in inp:
+			if "-" == inp[0]:
+				try:
+					lastAnswer = lastAnswer - float(numGet(inp[1:]))
+				except:
+					print("Invalid value")
 
-		if op == "+":
-			lastAnswer = float(num1) + float(num2)
+			else:
+				vals = inp.split("-")
+				lastAnswer = float(numGet(vals[0])) - float(numGet(vals[1]))
+
+			print(lastAnswer)	
+			continue
+		elif "*" in inp:
+			if "*" == inp[0]:
+				try:
+					lastAnswer = lastAnswer * float(numGet(inp[1:]))
+				except:
+					print("Invalid value")
+
+			else:
+				vals = inp.split("*")
+				lastAnswer = float(numGet(vals[0])) * float(numGet(vals[1]))
+
 			print(lastAnswer)
-		
-		elif op == "-":
-			lastAnswer = float(num1) - float(num2)
+			continue
+		elif "/" in inp:
+			if "/" == inp[0]:
+				try:
+					lastAnswer = lastAnswer / float(numGet(inp[1:]))
+				except:
+					print("Invalid value")
+
+			else:
+				vals = inp.split("/")
+				lastAnswer = float(numGet(vals[0])) / float(numGet(vals[1]))
+
 			print(lastAnswer)
-		
-		elif op == "/":
-			lastAnswer = float(num1) / float(num2)
-			print(lastAnswer)
-		
-		elif op == "*":
-			lastAnswer = float(num1) * float(num2)
-			print(lastAnswer)
-		
+			continue
 		else:
 			print("Invalid operator")
+			continue
 	
-	else:
+	elif inp == "m":
 		memory = lastAnswer	
+	else: print("Invalid input")
