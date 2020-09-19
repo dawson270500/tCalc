b19d65f09d144ccd57971a951ef52f1db1389306
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-09-19 11:11:14 +0100

    Added checks, fixed bug
    Added checks for if you put in invalid letters or values
    
    Fixed a bug to do with launching

--- calc.py
+++ calc.py
@@ -1,5 +1,3 @@
-import math
-
 lastAnswer = 0.0
 memory = 0.0
 
@@ -17,6 +15,7 @@ while True:
 	op = ""
 	if inp == "help":
 		print("You can add onto the last answer given, by typing '<operator><Number or value>'\nSupported operators:\n\t+\n\t-\n\t*\n\t/\nUsing pi or memory:\n\tto use pi, type p.\n\tTo set memory, type m after it gives the answer you want in memory. To acces memory type m in a calculation")
+	
 	elif inp != "m":
 		for x in inp:
 			if x == "+" or x == "-" or x == "/" or x=="*":
@@ -25,44 +24,65 @@ while True:
 					num1 = str(lastAnswer)
 					if inp[1] == "m":
 						num2 = memory
+					
 					elif inp[1] =="p":
 						num2 = 3.14159
+					
 					else:
 						num2 = inp[1:]
 					break
+				
 				else:
 					numSet = True
-			else:
+				continue
+			
+			if x.isnumeric() or x == "p" or x == "m":
 				if numSet == False:
 					if x == "p":
 						num1 = 3.14159
 						continue
+					
 					if x == "m":
 						num1 = memory
 						continue
 					num1 = num1+x
+				
 				else:
 					if x == "p":
 						num2 = 3.14159
 						continue
+					
 					if x == "m":
 						num2 = memory
 						continue
 					num2 = num2+x
+			else:
+				print("That isn't a number")
+				numSet = "e"
+				break
+
+		if numSet == "e":
+			continue			
+
 		if op == "+":
 			lastAnswer = float(num1) + float(num2)
 			print(lastAnswer)
+		
 		elif op == "-":
 			lastAnswer = float(num1) - float(num2)
 			print(lastAnswer)
+		
 		elif op == "/":
 			lastAnswer = float(num1) / float(num2)
 			print(lastAnswer)
+		
 		elif op == "*":
 			lastAnswer = float(num1) * float(num2)
 			print(lastAnswer)
+		
 		else:
 			print("Invalid operator")
+	
 	else:
 		memory = lastAnswer
 
