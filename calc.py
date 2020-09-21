5c9c1ae7b15a427ae6677fff31a18353105ba748
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-09-21 06:10:18 +0100

    Command Arguements, New operations
    Added the ability to do calculations from within the command, the number you can do is unlimited and it can use last answer
    
    Added Cos[ine], Tan[gent], and Sin[e]

--- calc.py
+++ calc.py
@@ -1,87 +1,134 @@
+import math
+import random
+import sys
+
 lastAnswer = 0.0
 memory = 0.0
 
-print("Type 'exit' to stop, 'help' for help")
+print("-- tCalc V1.2 -- Programmed by Bailey Dawson --")
 
-def numGet(token):
+def numGet(token):#get the number out of the string
 	if token == "m":
 		return memory
-	elif token == "p":
-		return 3.14159
-	else:
+	if token =="r":
+		return random.random()
+	if token == "p":
+		return math.pi
+	if str(token).isnumeric():
 		return token
+	return "ERROR"
+
+def getAnswer(inp):#Take in a string, convert it to a answer
+	if "+" in inp:
+			if "+" == inp[0]:
+				try:
+					num = numGet(inp[1:])
+					if num != "ERROR": return lastAnswer + float(num)
+					else: 
+						print("Invalid value | Code 1.1")
+						return "ERROR"
+				except:
+					print("Invalid value | Code 1.2")
+					return "ERROR"
+
+			else:
+				vals = inp.split("+")
+				return float(numGet(vals[0])) + float(numGet(vals[1]))
+	elif "-" in inp:
+		if "-" == inp[0]:
+			try:
+				num = numGet(inp[1:])
+				if num != "ERROR": return lastAnswer - float(num)
+				else: 
+					print("Invalid value | Code 2.1")
+					return "ERROR"
+			except:
+				print("Invalid value | Code 2.2")
+				return "ERROR"
+
+		else:
+			vals = inp.split("-")
+			return float(numGet(vals[0])) - float(numGet(vals[1]))
+	elif "*" in inp:
+		if "*" == inp[0]:
+			try:
+				num = numGet(inp[1:])
+				if num != "ERROR": return lastAnswer * float(num)
+				else:
+					print("Invalid value | Code 3.1") 
+					return "ERROR"
+			except:
+				print("Invalid value | Code 3.2")
+				return "ERROR"
+
+		else:
+			vals = inp.split("*")
+			return float(numGet(vals[0])) * float(numGet(vals[1]))
+	elif "/" in inp:
+		if "/" == inp[0]:
+			try:
+				num = numGet(inp[1:])
+				if num != "ERROR": return lastAnswer / float(num)
+				else: 
+					print("Invalid value | Code 4.1")
+					return "ERROR"		
+			except:
+				print("Invalid value | Code 4.2")
+				return "ERROR"
+
+		else:
+			vals = inp.split("/")
+			return float(numGet(vals[0])) / float(numGet(vals[1]))
+	elif "cos(" in inp:
+		vals = inp.split("(")
+		if vals[1] != "":
+			vals[1] = vals[1].replace(")", "")
+			num = numGet(vals[1])
+			if num != "ERROR": return math.cos(float(num))
+	elif "tan(" in inp:
+		vals = inp.split("(")
+		if vals[1] != "":
+			vals[1] = vals[1].replace(")", "")
+			num = numGet(vals[1])
+			if num != "ERROR": return math.tan(float(num))
+	elif "sin(" in inp:
+		vals = inp.split("(")
+		if vals[1] != "":
+			vals[1] = vals[1].replace(")", "")
+			num = numGet(vals[1])
+			if num != "ERROR": return math.sin(float(num))
+	return "Invalid Input | Code 0.2"
+
+if len(sys.argv) > 1:
+	del sys.argv[0]
+	for x in sys.argv:
+		lastAnswer = getAnswer(x)
+		print(lastAnswer)
+	sys.exit()
+
+print("Type 'exit' to stop, 'help' for help")
 
 while True:
 	inp = input(":> ").replace(" ", "")
 	
-	if inp == "exit":	
+	if inp == "exit":	#Stop
 		break
 
-	elif inp == "help":
-		print("You can add onto the last answer given, by typing '<operator><Number or value>'\nSupported operators:\n\t+\n\t-\n\t*\n\t/\nUsing pi or memory:\n\tto use pi, type p.\n\tTo set memory, type m after it gives the answer you want in memory. To acces memory type m in a calculation")
+	elif inp == "help": # Help
+		print("You can add onto the last answer given, by typing '<operator><Number or value>'\nSupported operators:\n\t+\n\t-\n\t*\n\t/\n\tcos(<val>)\n\tsin(<val>)\n\ttan(<val>)\nUsing pi or memory:\n\tto use pi, type p.\n\tTo set memory, type m after it gives the answer you want in memory. To acces memory type m in a calculation")
 		continue
 
-	elif inp == "m":
+	elif inp == "m": #Put last into memory
 		memory = lastAnswer	
 		continue
 	
-	else: 
-		if "+" in inp:
-			if "+" == inp[0]:
-				try:
-					lastAnswer = lastAnswer + float(numGet(inp[1:]))
-				except:
-					print("Invalid value")
-					continue
-
-			else:
-				vals = inp.split("+")
-				lastAnswer = float(numGet(vals[0])) + float(numGet(vals[1]))
-
+	else: #Calculation
+		lastAnswer = getAnswer(inp)
+		if lastAnswer != "ERROR": 
 			print(lastAnswer)
 			continue
-		elif "-" in inp:
-			if "-" == inp[0]:
-				try:
-					lastAnswer = lastAnswer - float(numGet(inp[1:]))
-				except:
-					print("Invalid value")
-					continue
-
-			else:
-				vals = inp.split("-")
-				lastAnswer = float(numGet(vals[0])) - float(numGet(vals[1]))
-
-			print(lastAnswer)	
-			continue
-		elif "*" in inp:
-			if "*" == inp[0]:
-				try:
-					lastAnswer = lastAnswer * float(numGet(inp[1:]))
-				except:
-					print("Invalid value")
-					continue
-
-			else:
-				vals = inp.split("*")
-				lastAnswer = float(numGet(vals[0])) * float(numGet(vals[1]))
-
-			print(lastAnswer)
-			continue
-		elif "/" in inp:
-			if "/" == inp[0]:
-				try:
-					lastAnswer = lastAnswer / float(numGet(inp[1:]))
-				except:
-					print("Invalid value")
-					continue
-
-			else:
-				vals = inp.split("/")
-				lastAnswer = float(numGet(vals[0])) / float(numGet(vals[1]))
-
-			print(lastAnswer)
-			continue
-		else:
-			print("Invalid operator")
-			continue
+		print("ERROR")
+		continue
+	print("Invalid Input | Code 0.1")
+	print("ERROR")
+			
